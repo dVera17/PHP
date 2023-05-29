3f24f06b9225565f0fbcc1ea66454d864a3e4878
dVera17 <dsvera174@gmail.com>
2023-05-29 16:57:31 -0500

    feat: :sparkles: Variables y constantes
    Maneras de declarar constantes y variables{

--- index.php
+++ index.php
@@ -10,3 +10,32 @@ printf("Hola %s", $texto);
 $texto = "Mundo"; 
 $mensaje = sprintf("Hola %s", $texto);
 echo $mensaje;
+
+// VARIABLES
+
+/**
+ * Las variables son definidas usando el simbolo $
+ */
+
+ //Variable númerica
+ $edad = 25;
+
+// Variable de texto;
+$nombre = "Santiago";
+
+// Variable booleana
+$esValido = true;
+echo var_dump($nombre);
+
+// CONSTANTES
+
+// Variable númerica
+define("PI", 3.1416);
+
+// Declarar una constante de texto
+define("SALUDO", "hola");
+
+// Declarar una constante booleana
+define("ESVALIDO", true);
+
+
