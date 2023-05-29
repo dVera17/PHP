39da60bf1df401b75609ccda873e39552a319bb5
dVera17 <dsvera174@gmail.com>
2023-05-29 17:01:42 -0500

    feat: :sparkles: Tipos de datos
    Tipos de datos y algunos ejemplos

--- index.php
+++ index.php
@@ -38,4 +38,22 @@ define("SALUDO", "hola");
 // Declarar una constante booleana
 define("ESVALIDO", true);
 
+// Boolean
+$esValido = true;
+echo var_dump($nombre);
+
+// ENTEROS
+$edad = 25;
+echo var_dump($edad);
+
+// FLOATS
+$float = 24.5;
+echo var_dump($float);
+
+// Strings
+$nombre = "Santiago";
+echo var_dump($nombre);
+
+$array = [];
+var_dump($array);
 
