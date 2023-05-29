bf68b4397860ca2692cd45bd541cc7a5fe371348
dVera17 <dsvera174@gmail.com>
2023-05-29 16:49:13 -0500

    feat: :tada: Inicializacion proyecto, Funciones para imprimir
    Inicializacion proyecto, Funciones para imprimir

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Funciones para imprimir en el html
+
+echo "Texto a imprimir";
+
+$texto = "Mundo";
+printf("Hola %s", $texto);
+
+$texto = "Mundo"; 
+$mensaje = sprintf("Hola %s", $texto);
+echo $mensaje;
