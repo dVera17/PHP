1e4e137bff0f53cd1db68f9d2badd53cce5c6201
dvera17 <dsvera174@gmail.com>
2023-05-29 20:52:51 -0500

    feat: :construction: Numeros y operadores
    Numeros y operadores

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ echo $mensaje;
  */
 
  //Variable númerica
- $edad = 25;
+$edad = 25;
 
 // Variable de texto;
 $nombre = "Santiago";
@@ -57,3 +57,77 @@ echo var_dump($nombre);
 $array = [];
 var_dump($array);
 
+// Números y operadores
+
+$numero1 = 20;
+$numero2 = 20;
+$numero3 = 20;
+$numero4 = 20;
+
+var_dump($numero1 > $numero2);
+echo "<br>";
+
+var_dump($numero1 < $numero2);
+echo "<br>";
+
+var_dump($numero1 >= $numero2);
+echo "<br>";
+
+var_dump($numero1 <= $numero2);
+echo "<br>";
+
+var_dump($numero2 == $numero3);
+echo "<br>";
+
+var_dump($numero2 == $numero4);
+echo "<br>";
+
+var_dump($numero2 === $numero4);
+echo "<br>";
+
+/**
+ * -1 si izquierda es menor
+ * 0 Si es igual
+ * 1 Si izquierda es mayor
+ */
+
+var_dump($numero1 <=> $numero2);
+echo "<br>";
+
+var_dump($numero2 <=> $numero3);
+echo "<br>";
+
+var_dump($numero2 <=> $numero1);
+echo "<br>";
+
+$nombreCLiente = "Campers Campuslands";
+
+// Conocer extensión de ub string
+echo strlen($nombreCLiente);
+var_dump($nombreCLiente);
+
+// Eliminar espacios en blanco
+$text = trim($nombreCLiente);
+echo strlen($text);
+
+// Convertir a Mayuscula
+echo strtoupper($nombreCLiente);
+
+// Convertir en minuscula
+echo strtolower($nombreCLiente);
+
+$mail1 = "correo@correo.com";
+$mail2 = "correo@correo.com";
+
+var_dump(strtolower($mail) === strtolower($mail1));
+echo str_replace('Juan', 'J', $nombreCLiente);
+
+// Revisar si existe un string o no;
+echo strpos($nombreCLiente, 'Pedro');
+
+$tipoCLiente = "Premium - Empresarial";
+echo "<br>";
+
+echo "El cliente " . $nombreCLiente . "es" . $tipoCLiente;
+
+echo "El cliente {$nombreCLiente} es {$tipoCLiente}";
